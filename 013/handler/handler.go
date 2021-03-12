8ac100f64b2e7fe83fc2985f01ccf6c73f73282a
Rendi K <rendi.krinaldi@gmail.com>
2021-03-12 09:30:21 +0700

    http get & post

---
+++ 013/handler/handler.go
@@ -0,0 +1,97 @@
+package handler
+
+import (
+	"golang-webdev/013/entity"
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	tmpl, err := template.ParseFiles(path.Join("views", "index.html"), path.Join("views", "layout.html"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening", http.StatusInternalServerError)
+		return
+	}
+
+	data := map[string]interface{}{
+		"title":   "I'm learning golang web",
+		"content": "I'm learning golang web from online course",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening", http.StatusInternalServerError)
+		return
+	}
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world, I'm learning golang web development"))
+}
+
+func MarioHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Mario from Nintendo"))
+}
+
+func ProductHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	idNumb, err := strconv.Atoi(id)
+
+	if err != nil || idNumb < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// fmt.Fprintf(w, "Product Page : %d", idNumb)
+	tmpl, err := template.ParseFiles(path.Join("views", "product.html"), path.Join("views", "layout.html"))
+
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening", http.StatusInternalServerError)
+		return
+	}
+
+	// data := map[string]interface{}{
+	// 	"content": idNumb,
+	// }
+
+	// data := entity.Product{ID: 1, Name: "Ertiga", Price: 220000000, Stock: 3}
+	data := []entity.Product{
+		{ID: 1, Name: "Ertiga", Price: 220000000, Stock: 11},
+		{ID: 2, Name: "Xpander", Price: 260000000, Stock: 8},
+		{ID: 3, Name: "Pajero Sport", Price: 470000000, Stock: 1},
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error is happening", http.StatusInternalServerError)
+		return
+	}
+}
+
+func PostGetHandler(w http.ResponseWriter, r *http.Request) {
+	method := r.Method
+
+	switch method {
+	case "GET":
+		w.Write([]byte("This is a GET request"))
+
+	case "POST":
+		w.Write([]byte("This is a POST request"))
+
+	default:
+		http.Error(w, "Error is happening", http.StatusBadRequest)
+	}
+}
