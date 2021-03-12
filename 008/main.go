f045cec6675cddf9885a3e3585750f3cce9729b9
Rendi K <rendi.krinaldi@gmail.com>
2021-03-12 09:28:45 +0700

    html layout template

---
+++ 008/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang-webdev/008/handler"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handler.HomeHandler)
+	mux.HandleFunc("/hello", handler.HelloHandler)
+	mux.HandleFunc("/mario", handler.MarioHandler)
+	mux.HandleFunc("/product", handler.ProductHandler)
+
+	log.Println("Starting web on port 8080")
+
+	err := http.ListenAndServe(":8080", mux)
+	log.Fatal(err)
+}
